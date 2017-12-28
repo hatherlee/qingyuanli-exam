5a1f3bf656afb429a3974d181bf883c36805144e
hatherlee <hatherlee@qq.com>
2017-12-28 18:50:52 +0800

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: []
+  },
+  {
+    path: 'student',
+    loadChildren: './student/student.module#StudentModule',
+  },
+  {
+    path: 'stock',
+    loadChildren: './stock/stock.module#StudentModule',
+  }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
